30cf337b68b3e010653a0dfadcabfc85a192cce4
shibuyahiroyuki <mk.9.q.kyu@gmail.com>
2019-09-24 10:16:12 +0900

    ログイン後にユーザーページにリダイレクトする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-
   # deviseコントローラーにストロングパラメータを追加する          
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    current_user # ログイン後に遷移するpathを設定
+  end
 
   protected
     def configure_permitted_parameters
