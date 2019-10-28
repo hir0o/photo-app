cc3f37b415ddd876a09be60a1d37b0f010c8326a
shibuyahiroyuki <mk.9.q.kyu@gmail.com>
2019-10-28 09:31:19 +0900

    css若干修正

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,14 @@ module PostsHelper
   def edited?(comment)
      "編集済み" unless comment.created_at == comment.updated_at
   end
+
+  def post_list_title
+    if @tag_name
+      "「#{@tag_name}」の検索結果"
+    elsif @search
+      "「#{@search}」の検索結果"
+    else
+      "投稿一覧"
+    end
+  end
 end
