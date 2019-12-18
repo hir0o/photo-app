48db8a0b6ef0f5159928d53cc9dc05d11485a8ac
shibuyahiroyuki <mk.9.q.kyu@gmail.com>
2019-12-18 14:35:12 +0900

    いいね、フォロー、回覧履歴、コメントのseedデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ users.each.with_index(1) do |user, n|
   user.profile_image = open("#{Rails.root}/db/sample/profile/profile-#{n}.jpg")
   user.save
   # 写真の投稿
-  1.upto(rand(6) + 1) do |n|
+  1.upto(6) do |n|
     title = Faker::Movie.quote
     description = Faker::Lorem.sentence
     pictures = [
@@ -38,5 +38,37 @@ users.each.with_index(1) do |user, n|
                   latitude: latitude,
                   longitude: longitude
     )
+  end
+end
+
+# リレーションシップ
+users = User.all
+user  = users.last
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow!(followed) }
+followers.each { |follower| follower.follow!(user) }
+
+# いいね
+users = User.order(:created_at)
+
+1.upto(10) do |i|
+  0.upto(10) do |n|
+    users[i].posts[0].likes.create(user_id: users[n].id)
+  end
+end
+
+# 回覧履歴
+1.upto(10) do |i|
+  0.upto(10) do |n|
+    users[i].posts[0].footprints.create(user_id: users[n].id)
+  end
+end
+
+#コメント
+1.upto(10) do |i|
+  10.upto(10) do |n|
+    content = Faker::Lorem.sentence
+    users[i].comments.create(post_id: n, content: content)
   end
 end
