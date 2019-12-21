3c5709e6f0c802582365ef593453162a9d058269
shibuyahiroyuki <mk.9.q.kyu@gmail.com>
2019-12-21 16:21:00 +0900

    コメントアウトを修正

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,13 +1,13 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "ホームのリクエストテスト", type: :request do
+describe "ホームのリクエストテスト", type: :request do
 
-#   describe "GET /" do
-#     before do
-#       get root_path
-#     end
-#     it "リクエストが成功すること" do
-#       expect(response.status).to eq(200)
-#     end
-#   end
-# end
+  describe "GET /" do
+    before do
+      get root_path
+    end
+    it "リクエストが成功すること" do
+      expect(response.status).to eq(200)
+    end
+  end
+end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -122,34 +122,4 @@ RSpec.describe "投稿のリクエストテスト", type: :request do
       end
     end
   end
-
-
-  # describe "ログインしている場合" do
-  #   before do
-  #     user = FactoryBot.create(:user1)
-  #     sign_in user
-  #   end
-
-  #   describe "" do
-  #     before do
-  #       get '/posts/new'
-  #     end
-
-  #     it "投稿ページが表示される" do
-  #       expect(response.status).to eq(200)
-  #     end
-  #   end
-  # end
-
-  # describe "ログインしていない場合" do
-  #   describe "/posts/newにアクセスすると" do
-  #     before do
-  #       get '/posts/new'
-  #     end
-
-  #     it "リダイレクトされること" do
-  #       expect(response.status).to eq(302)
-  #     end
-  #   end
-  # end
 end
