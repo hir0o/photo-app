00c870bdc21cdb1df45f32f385b04f21b385804a
shibuyahiroyuki <mk.9.q.kyu@gmail.com>
2020-01-29 19:28:55 +0900

    細かい修正

--- app/assets/javascripts/post_close.js
+++ app/assets/javascripts/post_close.js
@@ -2,3 +2,7 @@ $("#close").click(function() {
   $("#modal").remove();
   $("body").css("overflow", "visible");
 });
+$(".modal-dialog").click(function() {
+  $("#modal").remove();
+  $("body").css("overflow", "visible");
+});
